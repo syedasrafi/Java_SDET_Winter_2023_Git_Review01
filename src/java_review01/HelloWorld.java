5c3eddc0306555424d70fd90e7909ba16ebce6a1
syedasrafi <u.nahid5@gmail.com>
2024-01-20 18:29:31 -0500

    Github01

---
+++ src/java_review01/HelloWorld.java
@@ -0,0 +1,11 @@
+package java_review01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hi there");
+
+        System.out.println("This is a test");
+        System.out.println("Test is completed");
+    }
+}
